1e3ac7cd126ae03cb50662c82b58c49f09b14eef
Juan Olvera <jp.oes@hotmail.com>
2021-03-02 15:36:06 -0600

    add components and update react

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'crl-test'
+import { Button } from 'crl-test'
 import 'crl-test/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button label='submit' />
+      <Button label='submit' size='large' />
+      <Button label='submit' size='small' />
+    </>
+  )
 }
 
 export default App
